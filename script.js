57056c7e8c5d63711ba19ea56680621b72439b9c
sebabaik <sebamabaik@gmail.com>
2026-04-16 15:34:57 -0300

    agregue pantalon viscosa

--- script.js
+++ script.js
@@ -62,7 +62,7 @@ const TEMPORADAS = {
       { cat: 'remeras-ml',  label: 'Remeras ML',       icon: '👕' },
       { cat: 'buzos',       label: 'Buzos',            icon: '🧥' },
       { cat: 'sacos',       label: 'Sacos de paño',    icon: '🧥' },
-      { cat: 'joggings',    label: 'Joggings',         icon: '🩳' },
+      { cat: 'pantalones',    label: 'pantalones',         icon: '🩳' },
       { cat: 'camperas',    label: 'Camperas',         icon: '🧥' },
       { cat: 'chalecos',    label: 'Chalecos',         icon: '🦺' },
       { cat: 'ruana',     label: 'ruana',          icon: '🧣' },
